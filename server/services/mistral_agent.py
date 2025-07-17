d212109892a3697a770892bb46bc288376f30c63
YaelP <yaelparrac@gmail.com>
2025-07-17 08:25:16 +0200

    feat:(mistral_agent): added region variable

--- server/services/mistral_agent.py
+++ server/services/mistral_agent.py
@@ -4,7 +4,8 @@ from services.prompt_builder import get_combined_prompt
 user_input = "Comment développer son intelligence émotionnelle ?"    #   |
 platform = "instagram"                                               #   |
 age_range = "20-25"                                                  #   |
+region = "UK"                                                        #   |
 # -----------------------------------------------------------------------
 
-prompt_fro_mistral = get_combined_prompt(user_input, platform, age_range)
+prompt = get_combined_prompt(user_input, platform, age_range, region)
 # POLINA!!!! este es el prompt que debe de recibir mistral al que llamamos con la API de groq. En la caja hay un ejemplo tipo 
