11f45133953df25e5ca3dd986fe30351922c84a6
YaelP <yaelparrac@gmail.com>
2025-07-30 18:37:30 +0200

    feat: simple rag created and working fine. agent modified so it uses as 3th strategy this route

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from server.routes import basic_content, agent,image, yahoo
+from server.routes import basic_content, agent,image, yahoo, simple_rag_chroma
 from loguru import logger
 from server.routes.query_rag import router as query_router
 
@@ -9,9 +9,10 @@ app = FastAPI(title="LLM API", version="1.0.0")
 # These are all in server/routes folder
 app.include_router(basic_content.router)
 app.include_router(agent.router)
-app.include_router(query_router) # This is the RAG so it goes to arxiv search and retrieve info for chorma db
+app.include_router(query_router)            # This is the RAG so it goes to arxiv search and retrieve info for chorma database
 app.include_router(image.router) 
 app.include_router(yahoo.router) 
+app.include_router(simple_rag_chroma.router) # This is the simple rag that goes to the already existent info in chroma database
 
 @app.get("/")
 async def root():
